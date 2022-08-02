339c0ff767add495055edc71e91b1bf13025acb8
anakindinger <anakindinger@gmail.com>
2022-08-02 15:53:06 -0300

    primeiro commit

---
+++ estacionamento.py
@@ -0,0 +1,15 @@
+#REQUISITOS
+# O estacionamento é um pátio de apenas um andar e possui 50 vagas
+# Há 5 vagas para carros e 5 para motos, as vagas de carro são maiores que de moto
+# Carros e motos são identificados por suas placas
+# Vagas são identificadas por um número identificador único
+# Carros só podem ser estacionados em vagas de carro
+# Motos preferencialmente são estacionadas em vagas de motos, se não houverem vagas
+#disponíveis, podem ocupar vagas de carros
+# É preciso ter controle sobre qual carros está em qual vaga para agilizar a saída quando
+#o dono vem buscar
+# É preciso saber o número de vagas livres de carro e de moto para que o estacionamento
+#saiba se podem entrar novos carros e motos
+
+
+
